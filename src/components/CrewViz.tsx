c80c360c409abef18dac8300100ffb3115ea1c23
James Heldridge <jamesheldridge@unomaha.edu>
2026-02-12 14:53:03 -0600

    Crew HQ: match background to header color so letterboxing is invisible

--- src/components/CrewViz.tsx
+++ src/components/CrewViz.tsx
@@ -2,7 +2,13 @@
 
 export default function CrewViz() {
   return (
-    <div className="bg-[#08080c] -m-6 overflow-hidden" style={{ width: 'calc(100% + 48px)', height: 'calc(100vh - 64px)' }}>
+    <div 
+      className="-m-6 bg-[#0e0d14]" 
+      style={{ 
+        width: 'calc(100% + 48px)', 
+        height: 'calc(100vh - 56px)',
+      }}
+    >
       <iframe
         src="/crew-viz.html"
         className="w-full h-full border-0 block"
