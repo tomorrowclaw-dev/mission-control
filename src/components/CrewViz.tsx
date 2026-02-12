3f509a9b25fda184c643b67b7ba5b7563f9d6bd7
James Heldridge <jamesheldridge@unomaha.edu>
2026-02-12 14:36:10 -0600

    Clean up Crew HQ: fill container, remove black bars, center canvas

--- src/components/CrewViz.tsx
+++ src/components/CrewViz.tsx
@@ -2,12 +2,13 @@
 
 export default function CrewViz() {
   return (
-    <div className="rounded-xl overflow-hidden border border-[var(--border)]" style={{ height: 'calc(100vh - 60px)' }}>
+    <div className="flex items-center justify-center rounded-xl overflow-hidden bg-[#08080c]" style={{ height: 'calc(100vh - 100px)' }}>
       <iframe
         src="/crew-viz.html"
         className="w-full h-full border-0 block"
         title="OpenClaw Crew HQ"
         allow="autoplay"
+        style={{ maxWidth: '100%', maxHeight: '100%' }}
       />
     </div>
   )
