cd349f7aad4df6442281a9d35350b1b9b03d91c5
James Heldridge <jamesheldridge@unomaha.edu>
2026-02-12 14:47:03 -0600

    Crew HQ: use aspect ratio container to eliminate black bars

--- src/components/CrewViz.tsx
+++ src/components/CrewViz.tsx
@@ -2,13 +2,12 @@
 
 export default function CrewViz() {
   return (
-    <div className="flex items-center justify-center rounded-xl overflow-hidden bg-[#08080c]" style={{ height: 'calc(100vh - 100px)' }}>
+    <div className="w-full bg-[#08080c] rounded-xl overflow-hidden" style={{ aspectRatio: '480 / 320' }}>
       <iframe
         src="/crew-viz.html"
         className="w-full h-full border-0 block"
         title="OpenClaw Crew HQ"
         allow="autoplay"
-        style={{ maxWidth: '100%', maxHeight: '100%' }}
       />
     </div>
   )
