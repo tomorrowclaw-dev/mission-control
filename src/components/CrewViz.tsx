35d9140b8b7d7b6fe667d55a3318c6aa496e1e00
James Heldridge <jamesheldridge@unomaha.edu>
2026-02-12 15:18:58 -0600

    Crew HQ: increase max-width to 1400px

--- src/components/CrewViz.tsx
+++ src/components/CrewViz.tsx
@@ -7,7 +7,7 @@ export default function CrewViz() {
         className="rounded-xl overflow-hidden shadow-2xl"
         style={{
           width: '100%',
-          maxWidth: '1200px',
+          maxWidth: '1400px',
           aspectRatio: '3 / 2',
           background: '#0e0d14',
         }}
